bf9b7e4a6f0ced161013a9f497c85a4b0d3206ff
koritafei <koritafei@gmail.com>
2023-08-02 00:48:49 +0000

    inti

--- sylar/noncopyable.h
+++ sylar/noncopyable.h
@@ -1,3 +1,14 @@
+/**
+ * @file noncopyable.h
+ * @author koritafei (koritafei@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-07-28
+ *
+ * @copyright Copyright (c) 2023 by koritafei
+ *
+ */
+
 #ifndef __NONCOPYABLE__H__
 #define __NONCOPYABLE__H__
 
@@ -5,13 +16,12 @@ namespace sylar {
 
 class Noncopyable {
 public:
-  Noncopyable()  = default;
-  ~Noncopyable() = default;
+  Noncopyable()          = default;
+  virtual ~Noncopyable() = default;
 
-  Noncopyable(const Noncopyable &)            = delete;
-  Noncopyable &operator=(const Noncopyable &) = delete;
-
-private:
+  Noncopyable(const Noncopyable &)    = delete;
+  Noncopyable(Noncopyable &&)         = delete;
+  void operator=(const Noncopyable &) = delete;
 };
 
 }  // namespace sylar
--- sylar/singleton.h
+++ sylar/singleton.h
@@ -1,3 +1,14 @@
+/**
+ * @file singleton.h
+ * @author koritafei (koritafei@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-08-02
+ *
+ * @copyright Copyright (c) 2023 by koritafei
+ *
+ */
+
 #ifndef __SINGLETON__H__
 #define __SINGLETON__H__
 
@@ -8,28 +19,41 @@ namespace sylar {
 namespace {
 
 template <typename T, typename X, int N>
-T &GetInstanceX() {
-  static T x;
-  return x;
+T& GetInstanceX() {
+  static T v;
+  return v;
 }
 
 template <typename T, typename X, int N>
-std::shared_ptr<T> GetInstance() {
+std::shared_ptr<T> GetInstancePtr() {
   static std::shared_ptr<T> v(new T);
   return v;
 }
 
 }  // namespace
 
+/**
+ * @brief 单例模式封装类
+ *
+ * @tparam T 类型
+ * @tparam X 为了创造多个实例对应的Tag
+ * @tparam N 同一个Tag创造多个实例索引
+ */
 template <typename T, typename X = void, int N = 0>
 class Singleton {
 public:
-  static T *GetInstance() {
+  static T* GetInstance() {
     static T v;
     return &v;
   }
 };
 
+/**
+ * @brief 单例模式智能指针封装类
+ * @details T 类型
+ *          X 为了创造多个实例对应的Tag
+ *          N 同一个Tag创造多个实例索引
+ */
 template <typename T, typename X = void, int N = 0>
 class SingletonPtr {
 public:
